9fce1b8d1ea0ae0198dc59abfd68476ccfa9361e
guwenchang <1379864599@qq.com>
2019-05-14 19:42:10 +0800

    test

--- config/config.js
+++ config/config.js
@@ -1,11 +1,33 @@
 export default {
-    plugins: [
-        ['umi-plugin-react', {
-            // 这里暂时还没有添加配置，该插件还不会有作用，我们会在后面的课程按照需求打开相应的配置
-        }],
-    ],
-    routes: [{
+  plugins: [
+    ['umi-plugin-react', {
+      // 这里暂时还没有添加配置，该插件还不会有作用，我们会在后面的课程按照需求打开相应的配置
+      antd: true,
+      dva: true
+    }],
+  ],
+  routes: [{
+    path: '/',
+    component: '../layout',
+    routes: [
+      {
         path: '/',
-        component: './HelloWord',
-    }]
+        component: 'HelloWord'
+      },
+      {
+        path: '/helloworld',
+        component: 'HelloWord'
+      },
+      { path: 'puzzlecards', component: './puzzlecards' },
+
+      {
+        path: '/dashboard',
+        routes: [
+          { path: '/dashboard/analysis', component: 'Dashboard/Analysis' },
+          { path: '/dashboard/monitor', component: 'Dashboard/Monitor' },
+          { path: '/dashboard/workplace', component: 'Dashboard/Workplace' }
+        ]
+      },
+    ]
+  }],
 };
