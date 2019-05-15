0433398ec607f5c9c87b63834737e81bad29ba9b
guwenchang <1379864599@qq.com>
2019-05-15 17:48:56 +0800

    init

--- src/models/cards.js
+++ src/models/cards.js
@@ -1,24 +1,61 @@
+import * as cardsService from '../service/cards';
+
 export default {
-    namespace: 'cards',
-    state: {
-        cardsList: [{
-            id: 1,
-            name: '张三',
-            desc: '学习委员',
-            url: 'https://www.baidu.com'
-        }, {
-            id: 2,
-            name: '李四',
-            desc: '课代表',
-            url: 'https://www.google.com'
+
+  namespace: 'cards',
+
+  state: {
+    cardsList: [],
+    statistic: {},
+  },
+
+  effects: {
+    *queryList({ _ }, { call, put }) {
+      const rsp = yield call(cardsService.queryList);
+      console.log('queryList');
+      console.log(rsp);
+      yield put({ type: 'saveList', payload: { cardsList: rsp.result } });
+    },
+    *deleteOne({ payload }, { call, put }) {
+      const rsp = yield call(cardsService.deleteOne, payload);
+      console.log('deleteOne');
+      console.log(rsp);
+      yield put({ type: 'queryList' });
+      return rsp;
+    },
+    *addOne({ payload }, { call, put }) {
+      const rsp = yield call(cardsService.addOne, payload);
+      yield put({ type: 'queryList' });
+      return rsp;
+    },
+    *getStatistic({ payload }, { call, put }) {
+      const rsp = yield call(cardsService.getStatistic, payload);
+      yield put({
+        type: 'saveStatistic',
+        payload: {
+          id: payload,
+          data: rsp.result,
         },
-        ]
+      });
+      return rsp;
     },
-    reducers: {
-        queryList(state, { }) {
-            return {
-                ...state
-            };
-        }
+  },
+
+  reducers: {
+    saveList(state, { payload: { cardsList } }) {
+      return {
+        ...state,
+        cardsList,
+      }
     },
-}
+    saveStatistic(state, { payload: { id, data } }) {
+      return {
+        ...state,
+        statistic: {
+          ...state.statistic,
+          [id]: data,
+        },
+      }
+    },
+  },
+};
--- src/page/list/index.js
+++ src/page/list/index.js
@@ -1,42 +1,161 @@
 import React from 'react';
-import { Table } from 'antd';
+import { Table, Modal, Button, Form, Input } from 'antd';
 import { connect } from 'dva';
+import SampleChart from '../../component/SampleChart';
+
+const FormItem = Form.Item;
+
+class List extends React.Component {
+  state = {
+    visible: false,
+    statisticVisible: false,
+    id: null,
+  };
+
+  columns = [
+    {
+      title: '名称',
+      dataIndex: 'name',
+    },
+    {
+      title: '描述',
+      dataIndex: 'desc',
+    },
+    {
+      title: '链接',
+      dataIndex: 'url',
+      render(value) {
+        return (
+          <a href={value}>{value}</a>
+        );
+      },
+    },
+    {
+      title: '',
+      dataIndex: 'statistic',
+      render: (_, { id }) => {
+        return (
+          <Button onClick={() => { this.showStatistic(id); }}>图表</Button>
+        );
+      },
+    },
+    {
+      title: '操作',
+      dataIndex: 'delete',
+      render: (_, { id }) => {
+        return (
+          <Button onClick={() => { this.deleteItem(id); }}>删除</Button>
+        );
+      },
+    },
+  ];
+
+  componentDidMount() {
+    this.props.dispatch({
+      type: 'cards/queryList',
+    });
+  }
+
+  showModal = () => {
+    this.setState({ visible: true });
+  };
+
+  showStatistic = (id) => {
+    this.props.dispatch({
+      type: 'cards/getStatistic',
+      payload: id,
+    });
+    this.setState({ id, statisticVisible: true });
+  };
+
+  deleteItem = (id) => {
+    this.props.dispatch({
+      type: 'cards/deleteOne',
+      payload: id,
+    });
+  };
+
+  handleOk = () => {
+    const { dispatch, form: { validateFields,resetFields } } = this.props;
+
+    validateFields((err, values) => {
+      if (!err) {
+        dispatch({
+          type: 'cards/addOne',
+          payload: values,
+        });
+        console.log(values);
+        resetFields();
+        this.setState({ visible: false });
+      }
+    });
+  }
+
+  handleCancel = () => {
+    this.setState({
+      visible: false,
+    });
+  }
+
+  handleStatisticCancel = () => {
+    this.setState({
+      statisticVisible: false,
+    });
+  }
+
+  render() {
+    const { visible, statisticVisible, id } = this.state;
+    const { cardsList, cardsLoading, form: { getFieldDecorator }, statistic } = this.props;
+
+    return (
+      <div>
+        <Table columns={this.columns} dataSource={cardsList} loading={cardsLoading} rowKey="id" />
+
+        <Button onClick={this.showModal}>新建</Button>
+
+        <Modal
+          title="新建记录"
+          visible={visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+        >
+          <Form>
+            <FormItem label="名称">
+              {getFieldDecorator('name', {
+                rules: [{ required: true }],
+              })(
+                <Input />
+              )}
+            </FormItem>
+            <FormItem label="描述">
+              {getFieldDecorator('desc')(
+                <Input />
+              )}
+            </FormItem>
+            <FormItem label="链接">
+              {getFieldDecorator('url', {
+                rules: [{ type: 'url' }],
+              })(
+                <Input />
+              )}
+            </FormItem>
+          </Form>
+        </Modal>
+
+        <Modal visible={statisticVisible} footer={null} onCancel={this.handleStatisticCancel}>
+          <SampleChart data={statistic[id]} />
+        </Modal>
+      </div>
+    );
+  }
+}
 
 function mapStateToProps(state) {
-    return {
-        cardsList: state.cards.cardsList,
-        cardsLoading: state.loading.effects['cards/queryList'],
-    };
-}
-class List extends React.Component {
-    columns = [
-        {
-          title: '名称',
-          dataIndex: 'name',
-        },
-        {
-          title: '描述',
-          dataIndex: 'desc',
-        },
-        {
-          title: '链接',
-          dataIndex: 'url',
-          render: value => <a href={value}>{value}</a>,
-        },
-      ];
-    componentDidMount() {
-        this.props.dispatch({
-            type: 'cards/queryList',
-        });
-    }
-    render() {
-        const { cardsList, cardsLoading } = this.props;
-        return (
-          <div>
-            <Table columns={this.columns} dataSource={cardsList} loading={cardsLoading} rowKey="id" />
-          </div>
-        );
-      }
+  return {
+    cardsList: state.cards.cardsList,
+    cardsLoading: state.loading.effects['cards/queryList'],
+    statistic: state.cards.statistic,
+  };
 }
 
-export default connect(mapStateToProps)(List);
+export default connect(mapStateToProps)(Form.create()(List));
